c4457b20695155028233794aa8ef755b40ab2932
binyot <sxterminal@protonmail.com>
2020-02-23 17:46:29 +0300

    added function to file ops and platform bindings

--- botmem.c
+++ botmem.c
@@ -6,6 +6,11 @@
 #include <linux/types.h>
 #include <linux/uaccess.h>
 
+static int botmem_probe(struct platform_device *pdev);
+static int botmem_remove(struct platform_device *pdev);
+static ssize_t botmem_read(struct file *file, char *buffer, size_t len, loff_t *offset);
+static ssize_t botmem_write(struct file *file, const char *buffer, size_t len, loff_t *offset);
+
 struct botmem_dev {
     struct miscdevice miscdev;
     void __iomem *regs;
@@ -18,3 +23,22 @@ static struct of_device_id botmem_dt_ids[] = {
     },
     { }
 };
+
+MODULE_DEVICE_TABLE(of, botmem_dt_ids);
+
+static struct platform_driver botmem_platform = {
+    .probe = botmem_probe,
+    .remove = botmem_remove,
+    .driver = {
+        .name = "FOSSBot Memory Driver",
+        .owner = THIS_MODULE,
+        .of_match_table = botmem_dt_ids
+    }
+};
+
+static const struct file_operations botmem_fops = {
+    .owner = THIS_MODULE,
+    .read = botmem_read,
+    .write = botmem_write
+};
+
