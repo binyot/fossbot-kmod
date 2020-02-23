552ef11536c86af2cd03423b833586b2ea17409a
binyot <sxterminal@protonmail.com>
2020-02-23 17:30:00 +0300

    added makefiles

---
+++ Kbuild
@@ -0,0 +1,1 @@
+obj-m := botmem.o
---
+++ botmem.c
@@ -0,0 +1,8 @@
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/io.h>
+#include <linux/miscdevice.h>
+#include <linux/fs.h>
+#include <linux/types.h>
+#include <linux/uaccess.h>
+
